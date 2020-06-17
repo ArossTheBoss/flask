14039a699c7bd9f454b8dfb97762d526325c9b32
arossom1 <alexander.rossomakha@etrade.com>
2020-06-17 07:03:00 -0500

    updated warning

--- schemas/zephyr_test_plan_obj.py
+++ schemas/zephyr_test_plan_obj.py
@@ -3,14 +3,20 @@ from marshmallow import validate
 from helpers.schema_validation import Env
 
 
-class TestSchema(Schema):
+# Wrapper to set strict and override the warning in the inherited class
+class ApiSchemaWrapper(Schema):
+    class Meta:
+        strict = True
+
+
+class TestSchema(ApiSchemaWrapper):
     test_plan_key = fields.Str()
     test_type = fields.Str(validate=validate.OneOf(choices={'', 'AUTOMATED', 'automated', 'MANUAL', 'manual'},
                                                    error="value '{input}' is wrong. Should be one of: '{choices}'"))
     test_case_params = fields.Str()
 
 
-class TestsReportSchema(Schema):
+class TestsReportSchema(ApiSchemaWrapper):
     test_details = fields.Nested(TestSchema(many=True))
     should_add_unexecuted_cases = fields.Bool(default=False, missing=False)
     zephyr_env = fields.Str(required=False, default='prod', validate=Env(exclude=['invalid_env']))
